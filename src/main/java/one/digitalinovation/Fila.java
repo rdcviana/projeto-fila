e63822c8509f5f5cccb69c20836ea8bd3b0b4835
Rodrigo Viana <rodrigodcviana@gmail.com>
2023-03-23 14:01:19 -0300

    Classe Fila.java

---
+++ src/main/java/one/digitalinovation/Fila.java
@@ -0,0 +1,71 @@
+package one.digitalinovation;
+
+public class Fila {
+
+    private No refNoEntradaFila;
+
+    public Fila() {
+        this.refNoEntradaFila = null;
+    }
+
+    public boolean isEmpty() {
+        return refNoEntradaFila == null ? true : false;
+    }
+
+    public void enqueue(No novoNo) {
+        novoNo.setRefNo(refNoEntradaFila);
+        refNoEntradaFila = novoNo;
+    }
+
+    public No first() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    primeiroNo = primeiroNo.getRefNo();
+                } else {
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    public No dequeue() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+                } else {
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+            return primeiroNo;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "";
+        No noAuxiliar = refNoEntradaFila;
+        if (refNoEntradaFila != null) {
+            while (true) {
+                stringRetorno += "[No{objeto=" + noAuxiliar.getObj() + "}] ---> ";
+                if (noAuxiliar.getObj() != null) {
+                    noAuxiliar = noAuxiliar.getRefNo();
+                } else {
+                    stringRetorno += "null";
+                    break;
+                }
+            }
+        } else {
+            stringRetorno = "null";
+        }
+        return stringRetorno;
+    }
+}
