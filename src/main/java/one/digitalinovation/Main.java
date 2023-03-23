9b80cdb2acd4a5f23a322a25c4c9019e6390c27a
Rodrigo Viana <rodrigodcviana@gmail.com>
2023-03-23 15:44:38 -0300

    Utilização da classe Fila do projeto Estruturas de dados em Java

---
+++ src/main/java/one/digitalinovation/Main.java
@@ -0,0 +1,28 @@
+package one.digitalinovation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue(new No("primeiro"));
+        minhaFila.enqueue(new No("segundo"));
+        minhaFila.enqueue(new No("terceiro"));
+        minhaFila.enqueue(new No("quarto"));
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.dequeue());
+
+        System.out.println(minhaFila);
+
+        minhaFila.enqueue(new No("quinto"));
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.first());
+
+        System.out.println(minhaFila);
+
+    }
+}
